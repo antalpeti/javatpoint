d1f36f130b1af2f188227e024c2e44245f7fc442
Antal Péter <antal.peter@nn.hu>
2023-02-15 23:30:09 +0100

    Updated gitignore. Deleted initial class. Added examples of Java Method References topic.

---
+++ src/main/java/org/example/method/references/InstanceMethodReference2.java
@@ -0,0 +1,11 @@
+package org.example.method.references;
+
+public class InstanceMethodReference2 {
+    public void printnMsg(){
+        System.out.println("Hello, this is instance method");
+    }
+    public static void main(String[] args) {
+        Thread t2=new Thread(new InstanceMethodReference2()::printnMsg);
+        t2.start();
+    }
+}
